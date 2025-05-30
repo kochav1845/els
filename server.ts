c434912187ae8a47a7180cdd4677f90c5afaa3ba
kochav1845 <a88933513@gmail.com>
2025-05-30 04:00:10 -0400

    Update server.ts

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import { encode } from 'https://esm.sh/gpt-tokenizer@2.1.2'
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization', 
   'Access-Control-Max-Age': '86400',
 }
 
